61198f2ae3bfd42788f28b709ee6dc5ef986566a
thomaskovoor <thomaskovoor123@gmail.com>
2023-10-21 13:12:39 +0530

    demo tests

---
+++ src/test/java/com/example/cashcard/CashCardJsonTest.java
@@ -0,0 +1,13 @@
+package com.example.cashcard;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class CashCardJsonTest {
+
+@Test
+    public void myFirstTest() {
+        assertThat(42).isEqualTo(42);
+    }
+}
---
+++ src/test/java/com/example/cashcard/CashcardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cashcard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(classes = CashcardApplicationTests.class)
+class CashcardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
