42f11709762a713cb8df189b3d60b8ed7cbaa8b4
thomaskovoor <thomaskovoor123@gmail.com>
2023-10-21 23:36:03 +0530

    tests for correct and incorrect response

--- src/main/java/com/example/cashcard/CashCardController.java
+++ src/main/java/com/example/cashcard/CashCardController.java
@@ -2,16 +2,21 @@ package com.example.cashcard;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/cashcards")
-public class CashCardController{
+public class CashCardController {
 
     @GetMapping("/{requestedId}")
-    public ResponseEntity<String> findById() {
-        return ResponseEntity.ok("{}");
+    public ResponseEntity<CashCard> findById(@PathVariable Long requestedId) {
+        if(requestedId.equals(99L)){
+        CashCard cashCard = new CashCard(99L, 123.45);
+        return ResponseEntity.ok(cashCard);
+    }
+        else
+        return ResponseEntity.notFound().build();
      }
-    
 }
--- src/test/java/com/example/cashcard/CashcardApplicationTests.java
+++ src/test/java/com/example/cashcard/CashcardApplicationTests.java
@@ -6,21 +6,36 @@ import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
+import com.jayway.jsonpath.DocumentContext;
+import com.jayway.jsonpath.JsonPath;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class CashcardApplicationTests {
+    @Autowired
+    TestRestTemplate restTemplate;
 
-	@Autowired
-	TestRestTemplate restTemplate;
+    @Test
+    void shouldReturnACashCardWhenDataIsSaved() {
+        ResponseEntity<String> response = restTemplate.getForEntity("/cashcards/99", String.class);
 
-	@Test
-	void shouldReturnACashCardWhenDataIsSaved() {
-		ResponseEntity<String> response = restTemplate.getForEntity("/cashcards/99",String.class);
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+        DocumentContext documentContext = JsonPath.parse(response.getBody());
+Number id = documentContext.read("$.id");
+assertThat(id).isEqualTo(99);
 
-		assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+Double amount = documentContext.read("$.amount");
+assertThat(amount).isEqualTo(123.45);
 	}
 
+	@Test
+void shouldNotReturnACashCardWithAnUnknownId() {
+  ResponseEntity<String> response = restTemplate.getForEntity("/cashcards/1000", String.class);
+
+  assertThat(response.getStatusCode()).isEqualTo(HttpStatus.NOT_FOUND);
+  assertThat(response.getBody()).isBlank();
+}
 }
