05088da1495d23ed5733f5f02e9055c8c2f69178
thomaskovoor <thomaskovoor123@gmail.com>
2023-10-23 17:03:20 +0530

    added crud-repository,data.sql,schema.sql,h2

--- src/main/java/com/example/cashcard/CashCardController.java
+++ src/main/java/com/example/cashcard/CashCardController.java
@@ -1,5 +1,8 @@
 package com.example.cashcard;
 
+
+import java.util.Optional;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -10,13 +13,20 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/cashcards")
 public class CashCardController {
 
-    @GetMapping("/{requestedId}")
-    public ResponseEntity<CashCard> findById(@PathVariable Long requestedId) {
-        if(requestedId.equals(99L)){
-        CashCard cashCard = new CashCard(99L, 123.45);
-        return ResponseEntity.ok(cashCard);
-    }
-        else
-        return ResponseEntity.notFound().build();
+
+    private CashCardRepository cashCardRepository;
+    public CashCardController(CashCardRepository cashCardRepository) {
+        this.cashCardRepository = cashCardRepository;
+     }
+
+     @GetMapping("/{requestedId}")
+     public ResponseEntity<CashCard> findById(@PathVariable Long requestedId) {
+         Optional<CashCard> cashCardOptional = cashCardRepository.findById(requestedId);
+ 
+         if(cashCardOptional.isPresent())
+         return ResponseEntity.ok(cashCardOptional.get());
+         else
+         return ResponseEntity.notFound().build();
+         
      }
 }
