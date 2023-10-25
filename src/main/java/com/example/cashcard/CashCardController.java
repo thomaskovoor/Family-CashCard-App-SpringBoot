1887d5140fc2356a15ca69c82966dfdbe0656849
thomaskovoor <thomaskovoor123@gmail.com>
2023-10-25 16:29:00 +0530

    added post endpoint and tests

--- src/main/java/com/example/cashcard/CashCardController.java
+++ src/main/java/com/example/cashcard/CashCardController.java
@@ -1,13 +1,17 @@
 package com.example.cashcard;
 
 
+import java.net.URI;
 import java.util.Optional;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UriComponentsBuilder;
 
 @RestController
 @RequestMapping("/cashcards")
@@ -29,4 +33,14 @@ public class CashCardController {
          return ResponseEntity.notFound().build();
          
      }
+
+     @PostMapping()
+    private ResponseEntity<Void> createCashCard(@RequestBody CashCard newCashCardRequest,UriComponentsBuilder ucb){
+        CashCard savedCashCard = cashCardRepository.save(newCashCardRequest);
+        URI locationOfNewCashCard = ucb
+                 .path("cashcards/{id}")
+                 .buildAndExpand(savedCashCard.id())
+                 .toUri();
+        return ResponseEntity.created(locationOfNewCashCard).build();
+    }
 }
--- src/test/java/com/example/cashcard/CashcardApplicationTests.java
+++ src/test/java/com/example/cashcard/CashcardApplicationTests.java
@@ -11,6 +11,8 @@ import com.jayway.jsonpath.JsonPath;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.net.URI;
+
 
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -37,5 +39,24 @@ void shouldNotReturnACashCardWithAnUnknownId() {
 
   assertThat(response.getStatusCode()).isEqualTo(HttpStatus.NOT_FOUND);
   assertThat(response.getBody()).isBlank();
+}
+
+ @Test
+void shouldCreateANewCashCard() {
+   CashCard newCashCard = new CashCard(null, 250.00);
+   ResponseEntity<Void> createResponse = restTemplate.postForEntity("/cashcards", newCashCard, Void.class);
+   assertThat(createResponse.getStatusCode()).isEqualTo(HttpStatus.CREATED);
+
+   URI locationOfNewCashCard = createResponse.getHeaders().getLocation();
+   ResponseEntity<String> getResponse = restTemplate.getForEntity(locationOfNewCashCard,String.class);
+
+   assertThat(getResponse.getStatusCode()).isEqualTo(HttpStatus.OK);
+
+   DocumentContext documentContext = JsonPath.parse(getResponse.getBody());
+Number id = documentContext.read("$.id");
+Double amount = documentContext.read("$.amount");
+
+assertThat(id).isNotNull();
+assertThat(amount).isEqualTo(250.00);
 }
 }
